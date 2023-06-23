139eab271e8e4a8d59de0fe246b1a0c827860314
sudharshan1782003 <sudharsanvtt@gmail.com>
2023-06-23 17:45:37 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "react"
+import React from 'react'
+import Header from './header'
+import Additems from './Additems'
 
-function App() {
-
- const [item, setitem] = React.useState("")
-
- function Handleinput(e){
-   setitem(preval => preval.concat(e.target.name))
-   e.preventDefault()
- }
- function Equal(e){
-    setitem(preval => eval(preval))
-    e.preventDefault()
- }
- function Ac(e){
-  setitem(preval => preval = "")
-  e.preventDefault()
- }
- function Clear(e){
-  setitem(item.substring(0, item.length - 1))
-  e.preventDefault()
- }
-
-
-
-return(
-  <form className='w-80  bg-pink-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-3 rounded-md'>
-<input type='text' className='bg-blue-950 text-4xl w-72 ml-4 text-white text-right p-1' value={item} /><br></br>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='7'>7</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='8'>8</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='9'>9</button>
-<button className='bg-red-500 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Ac} >Ac</button><br></br>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='4'>4</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='5'>5</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='6'>6</button>
-<button className='bg-black p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Clear}>C</button><br></br>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='1'>1</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='2'>2</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='3'>3</button>
-<button className='bg-green-500 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Equal} >=</button><br></br>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='0'>0</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='00'>00</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='.'>.</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14'onClick={Handleinput} name='%'>%</button><br></br>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14' onClick={Handleinput} name='+'>+</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14' onClick={Handleinput} name='-'>-</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14' onClick={Handleinput} name='*'>*</button>
-<button className='bg-blue-950 p-3 w-14 mx-3 rounded-md hover:bg-gray-300 hover:text-black text-white mt-3 h-14' onClick={Handleinput} name='/'>/</button>
-
-
-</form>
-)
+const App = () => {
+  return (
+    <main className='p-5'>
+   
+   <Header />
+   <Additems />
+  
+   </main>
+  )
 }
 
-export default App;
+export default App
--- src/header.js
+++ src/header.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Header(){
     return(
-        <header className='flex h-30 bg-purple-900 p-5 text-white'>
-        <img src='../Troll Face.png' className='w-10 pr-2' />
-        <h1 className='font-bold text-lg'>Meme Generator</h1>
-        <p className='ml-auto'>React Project-3</p>
-      </header>
+        <header>
+          <div className="bg-purple-100 w-full text-center py-5 font-bold text-gray text-2xl rounded-md">To do list App</div>
+        </header>
     )
 }
